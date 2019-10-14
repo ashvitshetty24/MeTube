75360f2d63d2724b4a7559a3a3a44f4f59128a78
ashvitshetty24 <38062777+ashvitshetty24@users.noreply.github.com>
2019-10-14 17:49:31 -0400

    Add files via upload

---
+++ contact-us.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Metube</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/styles.css">
+
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="css/main.css">
+
+        <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+    </head>
+    <body>
+                <div id = "wrap">
+
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+
+        <nav style = "background-color:#F66733;" class="navbar navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+	   <a href="#" class="pull-left"><img class = "img-responsive" src="512x512bb.jpg" height="68" width="71"></a>
+            <a class="navbar-brand" style = "color:#fff; font-size:40px;" href="index.php">Metube</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+
+        </div><!--/.navbar-collapse -->
+      </div>
+    </nav>
+                    <br>
+                    <br><br><br>
+
+<div class = 'container'>
+    <h1>Contact</h1>
+    <div class ="row">
+        <div class ="col-md-4">
+           <h4>Ashvit Shetty</h4>
+            <a href = "mailto:ashvits@clemson.edu">ashvits@clemson.edu</a>
+        </div>
+        <div class ="col-md-4">
+        </div>
+        <div class ="col-md-4">
+        <h4>Hameeda Dildar Taj</h4>
+        <a href = "mailto:htaj@clemson.edu">htaj@clemson.edu</a>
+
+
+        </div>
+    </div>
+    <br>
+
+    <div class ="row">
+        <div class ="col-sm-3"></div>
+        <div class ="col-sm-4">
+          <h4><b> Drop us a note </b></h4>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d947.7122666632317!2d-82.83443345691728!3d34.67696690613677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88585e6c50afb71b%3A0x839f18da00060102!2sDaniel+Hall!5e0!3m2!1sen!2sus!4v1553971496687!5m2!1sen!2sus" width="600" height="450" align="center" frameborder="0" style="border:1" allowfullscreen></iframe>
+        </div>
+
+    </div>
+
+
+</div>
+
+
+<?php include_once 'parts/footer.php'; ?>
